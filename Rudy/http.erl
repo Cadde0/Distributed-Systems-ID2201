80b7da8bceffc04963f55bb0c2e2bf0208bb0b38
Cadde0 <ocaddei@gmail.com>
2026-09-09 08:52:21 +0200

    Implemented Basic single client http server

---
+++ Rudy/http.erl
@@ -0,0 +1,68 @@
+-module(http).
+-export([parse_req/1, ok/1, get/1]).
+
+parse_req(R0) ->
+    {Request, R1} = req_line(R0),
+    {Headers, R2} = headers(R1),
+    {Body, _} = msg_body(R2),
+    {Request, Headers, Body}.
+
+
+%pattern matching "GET_" in the input
+req_line([$G, $E, $T, 32 | R0]) ->
+    {URI, R1} = req_uri(R0),
+    {Version, R2} = http_version(R1),
+    %pattern matching the CRLF at the end of the request line
+    [13, 10 | R3] = R2,
+    {{get, URI, Version}, R3}.
+%TODO: Add support for other HTTP methods (POST, PUT, DELETE, etc.)
+% TODO: If not \r\n, return an error or handle the error appropriately.
+
+
+%Base case, when the next character is a space(32), the URI is done.
+req_uri([32 | R0]) ->
+    {[], R0};
+%Recursive Case, when the next character is not a space(32), we add it to the URI and continue parsing.
+req_uri([C | R0]) ->
+    {RestURI, R1} = req_uri(R0),
+    {[C | RestURI], R1}.
+%TODO: Optimization: make this tail recursive, so we don't have to build up a list of characters and then reverse it at the end. Instead, we can build up the URI as we go and return it when we're done.
+% 
+%TODO: Parse the URI into its components (path, query string, etc.) and return them as a tuple instead of just a list of characters. This will make it easier to handle the request later on.
+% 
+%Checks for HTTP/1.1 or HTTP/1.0 otherwise throws an error
+http_version([$H, $T, $T, $P, $/, $1, $., $1 | R0]) ->
+    {http_1_1, R0};
+http_version([$H, $T, $T, $P, $/, $1, $., $0 | R0]) ->
+    {http_1_0, R0};
+http_version(_) ->
+    {error, unsupported_http_version}.
+
+
+
+%Collect chars until we hit a \r\n blank line, which indicates the end of the headers section.
+headers([13, 10 | R0]) ->
+    {[], R0};
+%Recursive case, when the next two characters are not \r\n, we parse the next header and continue parsing.
+headers(R0) ->
+    {Header, R1} = header(R0),
+    {RestHeaders, R2} = headers(R1),
+    {[Header | RestHeaders], R2}.
+
+%Base case, when the next two characters are \r\n, the header is done.
+header([13, 10 | R0]) ->
+    {[], R0};
+%Recursive case, when the next two characters are not \r\n, we add the next character to the header and continue parsing.
+header([C | R0]) ->
+    {RestHeader, R1} = header(R0),
+    {[C | RestHeader], R1}.
+
+%"The rest is body" - we don't care about the body for now, so we just return an empty list. Maybe fix this later
+msg_body(R0) ->
+    {R0, []}.
+
+ok(Body) ->
+    "HTTP/1.1 200 OK\r\n" ++ "\r\n" ++ Body.
+
+get(URI) ->
+    "GET " ++ URI ++ " HTTP/1.1\r\n" ++ "\r\n".
---
+++ Rudy/rudy.erl
@@ -0,0 +1,45 @@
+-module(rudy).
+-export([init/1]).
+
+init(Port) ->
+    %Deliver as list so we can pattern match on it
+    % active false means we will have to explicitly call gen_tcp:recv to get data from the socket
+    % reuseaddr true allows us to reuse the port if the server is restarted quickly
+    Opt = [list, {active, false}, {reuseaddr, true}],
+    case gen_tcp:listen(Port, Opt) of
+        {ok, ListenSocket} ->
+            %Handles one connection at a time, but we can easily change this to handle multiple connections by spawning a new process for each connection.
+            handle_connections(ListenSocket),
+            gen_tcp:close(ListenSocket),
+            ok;
+        {error, Error} ->
+            io:format("Failed to listen on port ~p: ~w~n", [Port, Error])
+    end.
+
+handle_connections(ListenSocket) ->
+    %gen_tcp:accept will block until a new connection is made, so we can just call it in a loop to handle multiple connections.
+    case gen_tcp:accept(ListenSocket) of
+        %When a a client connects, a new socket is created for that client, and we can use that socket to communicate with the client.
+        {ok, ClientSocket} ->
+            handle_request(ClientSocket);
+        {error, Error} ->
+            io:format("Failed to accept connection: ~w~n", [Error])
+    end.
+
+handle_request(ClientSocket) ->
+    %We use gen_tcp:recv to receive data from the client socket. The second argument is the number of bytes to receive, 0 means we want to receive all available data.
+    Recv = gen_tcp:recv(ClientSocket, 0),
+    case Recv of
+        {ok, Data} ->
+            Request = http:parse_req(Data),
+            Response = handle_response(Request),
+            gen_tcp:send(ClientSocket, Response);
+        {error, Error} ->
+            io:format("rudy: error: ~w~n", [Error])
+    end,
+    gen_tcp:close(ClientSocket).
+
+
+
+handle_response({{get, URI, _Version}, _Headers, _Body}) ->
+    http:ok("Hello " ++ URI).
